2f27f77cc6c7225cb29f08c8808a97d20886059c
Airat-dev-prog <eprst123@mail.ru>
2020-06-25 18:36:50 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -13,6 +13,9 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
 
     fun listenAnswer(answer:String):Pair<String,Triple<Int,Int,Int>> {
         return when {
+            /*question.isValid(answer).isNotEmpty() -> {
+                question.isValid(answer) to status.color
+            }*/
             question.answer.contains(answer.toLowerCase()) -> {
                 question = question.nextQuestion()
                 "Отлично - ты справился\n${question.question}" to status.color
@@ -33,7 +36,7 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
         NORMAL(Triple(255,255,255)),
         WARNING(Triple(255,120,0)),
         DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0));
+        CRITICAL(Triple(255,0,0));
         fun nextStatus():Status{
             return if (this.ordinal < values().lastIndex){
                 values()[this.ordinal + 1]
